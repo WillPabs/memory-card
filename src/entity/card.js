212d8e30956767090ec598920479463f2c6895a7
Will Pabs <pabiterowilliam@yahoo.com>
2022-10-24 13:39:38 -0400

    update object creation with proto

--- src/entity/card.js
+++ src/entity/card.js
@@ -1,9 +1,11 @@
 const Card = (type, image, label) => {
-    let obj = Object.create();
+    let obj = Object.create(cardFunctions);
     obj.type = type;
     obj.image = image;
     obj.label = label;
     return obj
 }
+
+const cardFunctions = {}
 
 export default Card;
