2bc3adf34cb78f9abf6857f47e74a62ce5d4e084
Will Pabs <pabiterowilliam@yahoo.com>
2022-10-24 15:37:33 -0400

    add logs to test api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import { getPokemonData } from './api/api';
+
+const data = getPokemonData();
+console.log(data)
+const poke = 
+  <div>
+    {data.map((element) => {
+      return (
+        <div>
+          {element}
+        </div>
+      )
+    })}
+  </div>
 
 function App() {
   return (
     <div className="App">
+      {poke}
     </div>
   );
 }
