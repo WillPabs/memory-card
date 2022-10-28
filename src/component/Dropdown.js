7601b1037049062ae4cc89d6f6487f7b49b3abb0
Will Pabs <pabiterowilliam@yahoo.com>
2022-10-28 15:18:22 -0400

    add css

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -25,7 +25,7 @@ const Dropdown = ({ types, onSelect }) => {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 {types.map(type => {
                     return (
                         <div key={type} className="py-1">
@@ -37,7 +37,7 @@ const Dropdown = ({ types, onSelect }) => {
                                             active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                                             'block px-4 py-2 text-sm'
                                         )}
-                                        onClick={onSelect}
+                                        onClick={onSelect}                                        
                                     >
                                         {type}
                                     </a>
