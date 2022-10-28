7d7b001aef5e8178ca6f791482ad2417d096002b
Will Pabs <pabiterowilliam@yahoo.com>
2022-10-28 15:18:52 -0400

    add css

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,7 +1,7 @@
 const Card = ({ name, imgUrl, onSelect }) => {
     return (
-        <figure id={name} onClick={onSelect}>
-            <img src={imgUrl} alt={name}></img>
+        <figure id={name} onClick={onSelect} className="cursor-pointer">
+            <img src={imgUrl} alt={name} className="rounded-sm"></img>
             <figcaption>{name}</figcaption>
         </figure>
     )
