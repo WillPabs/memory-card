9dc836b73bf50bdf1b14bf35785ba52560d16adf
Will Pabs <pabiterowilliam@yahoo.com>
2022-10-31 15:16:36 -0400

    styled to center

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ name, imgUrl, onSelect }) => {
     return (
-        <figure id={name} onClick={onSelect} className="cursor-pointer">
+        <figure id={name} onClick={onSelect} className="cursor-pointer flex flex-col justify-center">
             <img src={imgUrl} alt={name} className="rounded-sm"></img>
             <figcaption>{name}</figcaption>
         </figure>
