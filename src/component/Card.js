3159500326527e15963c19972ddbf4c19edf60ba
Will Pabs <pabiterowilliam@yahoo.com>
2022-11-02 15:22:58 -0400

    styled as rounded dark card

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,8 +1,8 @@
 const Card = ({ name, imgUrl, onSelect }) => {
     return (
-        <figure id={name} onClick={onSelect} className="cursor-pointer flex flex-none flex-col items-center justify-center">
-            <img src={imgUrl} alt={name} className="flex max-w-lg"></img>
-            <figcaption className="flex font-bold">{name}</figcaption>
+        <figure id={name} onClick={onSelect} className="flex flex-col m-2 align-center w-full max-w-sm rounded-lg shadow-md bg-gray-800 border-gray-700 cursor-pointer">
+            <img src={imgUrl} alt={name} className="flex m-8 rounded-xl"></img>
+            <figcaption className="flex font-bold pb-8 justify-center text-white">{name}</figcaption>
         </figure>
     )
 }
