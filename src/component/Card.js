5cbcbc8885c978105fd37bf2df9ef0f582495380
Will Pabs <pabiterowilliam@yahoo.com>
2022-11-15 15:05:25 -0500

    centered img

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,7 +1,7 @@
 const Card = ({ name, imgUrl, onSelect }) => {
     return (
         <figure id={name} onClick={onSelect} className="col-span-1 basis-1/4 m-2 max-w-sm align-items justify-between rounded-lg shadow-md bg-gray-800 border-gray-700 cursor-pointer">
-            <img src={imgUrl} alt={name} className="flex rounded-xl"></img>
+            <img src={imgUrl} alt={name} className="mx-auto rounded-lg"></img>
             <figcaption className="flex font-bold pb-8 justify-center text-center text-white">{name}</figcaption>
         </figure>
     )
