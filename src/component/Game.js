bf9e07c5d7b7456320963216d869120727376d13
Will <pabiterowilliam@yahoo.com>
2022-11-27 02:54:16 -0500

    track correct typeHighScore when changing types

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,30 +1,35 @@
 import { useState } from "react"
 import { getAnimalsData, getDisneyCharactersData, getPokemonData } from "../api/api";
+import Type from "../entity/type";
 import Cards from "./Cards";
 import Dropdown from "./Dropdown";
 import Scoreboard from "./Scoreboard";
 
 const Game = () => {
+    const pokemon = Type('pokemon', 0);
+    const animals = Type('animals', 0);
+    const disney = Type('disney', 0);
     const [cards, setCards] = useState([]);
     const [selectedCards, setSelectedCards] = useState([]);
     const [score, setScore] = useState(0);
-    const [typeHighScore, setTypeHighScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
-    const [types, setTypes] = useState(['pokemon', 'animals', 'disney']);
-    const [selectedType, setSelectedType] = useState('');
+    const [types, setTypes] = useState([pokemon, animals, disney]);
+    const [selectedType, setSelectedType] = useState({});
 
     const selectType = async (e) => {
         const type = e.target.textContent;
         let newCards;
         if (type === 'pokemon') {
             newCards = await getPokemonData();
+            setSelectedType(types[0]);
         } else if (type === 'animals') {
             newCards = await getAnimalsData();
+            setSelectedType(types[1]);
         } else if (type === 'disney') {
             newCards = await getDisneyCharactersData();
+            setSelectedType(types[2]);
         }
         setScore(0);
-        setSelectedType(type);
         setCards(newCards);
     };
 
@@ -32,7 +37,7 @@ const Game = () => {
         const name = e.target.parentNode.id;
         if (selectedCards.includes(name)) {
             if (score > highScore) setHighScore(score);
-            if (score > typeHighScore) setTypeHighScore(score);
+            if (score > selectedType.highScore) selectedType.setHighScore(score);
             setScore(0)
             setSelectedCards([])
         } else {
@@ -54,7 +59,7 @@ const Game = () => {
     return (
         <div className="flex flex-col px-20 self-center justify-items-center">
             <Dropdown types={types} onSelect={selectType}></Dropdown>
-            <Scoreboard score={score} typeHighScore={typeHighScore} highScore={highScore} type={selectedType}></Scoreboard>
+            <Scoreboard score={score} typeHighScore={selectedType.highScore} highScore={highScore} type={selectedType.name}></Scoreboard>
             <Cards cards={cards} onSelect={select}></Cards>
         </div>
     )
